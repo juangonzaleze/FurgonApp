1e5c16e0bb6323c72e1bb5f9c68d248bbec4b5b1
juangonzaleze <juan.gonzalez@astrapp.com>
2018-05-25 04:08:31 -0430

    LandingPage

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// toggle class scroll 
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50)
+    {
+    	$('#img-brand').removeClass('img-brand');
+        $('#img-brand').addClass('afterscroll');
+
+    } else
+    {
+        $('#img-brand').addClass('img-brand');
+        $('#img-brand').removeClass('afterscroll');
+    }  
+
+});
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 250)
+    {
+        $('#icon-hover').addClass('animation');
+    }
+});
+
+
+  
+// demo only 
+// open link in new tab without ugly target="_blank"
+// $("a[href^='http']").attr("target", "_blank");
+
+
+// Slider screemshoot
+
+$('#screemshoot').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
